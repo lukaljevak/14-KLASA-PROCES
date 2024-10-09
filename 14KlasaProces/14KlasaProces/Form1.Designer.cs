d7e6819fad1e5f726eb27ee703db0ccca6798e91
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-10-09 09:02:29 +0200

    Add files via upload

---
+++ 14KlasaProces/14KlasaProces/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _14KlasaProces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonIExplorer = new System.Windows.Forms.Button();
+            this.buttonExcel = new System.Windows.Forms.Button();
+            this.buttonWord = new System.Windows.Forms.Button();
+            this.buttonStartInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonIExplorer
+            // 
+            this.buttonIExplorer.Location = new System.Drawing.Point(115, 71);
+            this.buttonIExplorer.Name = "buttonIExplorer";
+            this.buttonIExplorer.Size = new System.Drawing.Size(217, 48);
+            this.buttonIExplorer.TabIndex = 0;
+            this.buttonIExplorer.Text = "Pokrenite Internet Explorer";
+            this.buttonIExplorer.UseVisualStyleBackColor = true;
+            this.buttonIExplorer.Click += new System.EventHandler(this.buttonIExplorer_Click);
+            // 
+            // buttonExcel
+            // 
+            this.buttonExcel.Location = new System.Drawing.Point(402, 71);
+            this.buttonExcel.Name = "buttonExcel";
+            this.buttonExcel.Size = new System.Drawing.Size(217, 48);
+            this.buttonExcel.TabIndex = 1;
+            this.buttonExcel.Text = "Pokrenite Excel";
+            this.buttonExcel.UseVisualStyleBackColor = true;
+            this.buttonExcel.Click += new System.EventHandler(this.buttonExcel_Click);
+            // 
+            // buttonWord
+            // 
+            this.buttonWord.Location = new System.Drawing.Point(115, 212);
+            this.buttonWord.Name = "buttonWord";
+            this.buttonWord.Size = new System.Drawing.Size(217, 48);
+            this.buttonWord.TabIndex = 2;
+            this.buttonWord.Text = "Pokrenite Word";
+            this.buttonWord.UseVisualStyleBackColor = true;
+            this.buttonWord.Click += new System.EventHandler(this.buttonWord_Click);
+            // 
+            // buttonStartInfo
+            // 
+            this.buttonStartInfo.Location = new System.Drawing.Point(402, 212);
+            this.buttonStartInfo.Name = "buttonStartInfo";
+            this.buttonStartInfo.Size = new System.Drawing.Size(217, 48);
+            this.buttonStartInfo.TabIndex = 3;
+            this.buttonStartInfo.Text = "StartInfo";
+            this.buttonStartInfo.UseVisualStyleBackColor = true;
+            this.buttonStartInfo.Click += new System.EventHandler(this.buttonStartInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStartInfo);
+            this.Controls.Add(this.buttonWord);
+            this.Controls.Add(this.buttonExcel);
+            this.Controls.Add(this.buttonIExplorer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonIExplorer;
+        private System.Windows.Forms.Button buttonExcel;
+        private System.Windows.Forms.Button buttonWord;
+        private System.Windows.Forms.Button buttonStartInfo;
+    }
+}
+
---
+++ 14KlasaProces/14KlasaProces/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14KlasaProces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonIExplorer_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+        }
+
+        private void buttonExcel_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+        }
+
+        private void buttonWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("winword.exe");
+        }
+
+        private void buttonStartInfo_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+            Process.Start(startInfo);
+        }
+    }
+}
